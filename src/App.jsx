817f7b07ca11d32bcc60263275da6ff2af055b99
Isahan <145161717+Isahannn@users.noreply.github.com>
2025-05-21 00:24:17 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Layout from "./pages/Common/Layout";
 import Main from "./pages/Main/Main";
 import Map from "./pages/Map/Map";
 import News from "./pages/News/News";
-import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
 import NotFound from "./pages/NotFound/NotFound";
 import { MyProvider } from "./context";
 
@@ -18,8 +16,6 @@ const router = createBrowserRouter([
       { path: "/", element: <Main />, index: true },
       { path: "/map", element: <Map /> },
       { path: "/news", element: <News /> },
-      { path: "/about", element: <About /> },
-      { path: "/contact", element: <Contact /> },
       { path: "*", element: <NotFound /> },
     ],
   },
